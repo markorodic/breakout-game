e300b067eef7c5e485347c9b798a594d3ffaeca5
Marko Rodic <mrmarkorodic@googlemail.com>
2017-06-29 23:30:02 +0100

    created a bouncing ball

--- js/script-min.js
+++ js/script-min.js
@@ -1,1 +1,1 @@
-var canvas=document.getElementById("myCavas"),ctx=canvas.getContext("2d");
+function drawBall(){ctx.beginPath(),ctx.arc(x,y,ballRadius,0,2*Math.PI),ctx.fillStyle="#0095DD",ctx.fill(),ctx.closePath()}function draw(){ctx.clearRect(0,0,canvas.width,canvas.height),drawBall(),x+=dx,y+=dy,(y+dy>canvas.height-ballRadius||y+dy<ballRadius)&&(dy=-dy),(x+dx>canvas.width-ballRadius||x+dx<ballRadius)&&(dx=-dx)}var canvas=document.getElementById("myCanvas"),ctx=canvas.getContext("2d"),x=canvas.width/2,y=canvas.height-30,dx=2,dy=-2,ballRadius=10;setInterval(draw,10);
--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
-var canvas = document.getElementById("myCavas")
-var ctx = canvas.getContext("2d")
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2
+var y = canvas.height-30
+var dx = 2
+var dy = -2
+var ballRadius = 10
+
+
+function drawBall() {
+	ctx.beginPath()
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2)
+	ctx.fillStyle = "#0095DD"
+	ctx.fill()
+	ctx.closePath()
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	drawBall()
+	x += dx
+	y += dy
+	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+		dy = -dy
+	}
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		dx = -dx
+	}
+}
+
+setInterval(draw, 10)
+
+
+// ctx.beginPath()
+// ctx.arc(80, 40, 20, 0, Math.PI*2, false)
+// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)"
+// ctx.stroke()
+// ctx.closePath()
+
